ab8c3911208573ad89e8923ec476f7f4bbdef31a
NV <noelia.vivas@itscordoba.edu.ar>
2025-06-09 18:27:53 -0300

    Js - formularios

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const descubrimiento = document.getElementById('descubrimiento');
+    const especifiqueContainer = document.getElementById('especifique-container');
+
+    descubrimiento.addEventListener('change', function () {
+        if (descubrimiento.value === 'otro') {
+            especifiqueContainer.style.display = 'block';
+        } else {
+            especifiqueContainer.style.display = 'none';
+        }
+    });
+});
+
